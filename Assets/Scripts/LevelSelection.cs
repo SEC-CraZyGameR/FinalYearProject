7724b0c2488d1f293a7a1dad583512dd9584aa51
fpgl-amanulla <mdamanulla44@gmail.com>
2021-02-12 00:50:51 +0600

    Level Selection Panel WIP.

---
+++ Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    #region Singleton
+    static LevelSelection _instance;
+    public static LevelSelection Instance
+    {
+        get
+        {
+            if (_instance)
+                return _instance;
+
+            return null;
+        }
+    }
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion
+
+    [Header("Buttons")]
+    [SerializeField] Button btnLeft;
+    [SerializeField] Button btnRight;
+
+    [Header("LevelData Scriptable Object")]
+    [SerializeField] LevelData_SO levelData_SO;
+
+    public List<GameObject> levelSelectionItems = new List<GameObject>();
+
+
+
+    void LeftBtnCallBack()
+    {
+
+    }
+
+    void RightBtnCallBack()
+    {
+
+    }
+
+    void ShowLevelItem(int _startIndex = 0)
+    {
+        int itemIndex = 0;
+        for (int i = _startIndex; i < _startIndex + 3; i++)
+        {
+            ILevelSelection item = levelSelectionItems[itemIndex++].GetComponent<ILevelSelection>();
+            item.AssignValue(levelData_SO.AllLevelInfo[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSelectionItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILevelSelection
+{
+    void AssignValue(LevelInfo _levelInfo);
+}
+
+public class LevelSelectionItem : MonoBehaviour, ILevelSelection
+{
+    public void AssignValue(LevelInfo _levelInfo)
+    {
+        throw new System.NotImplementedException();
+    }
+}
