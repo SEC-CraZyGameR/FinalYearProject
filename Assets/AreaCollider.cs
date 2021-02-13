53869ea35d27719aba3aeb0fd065cf6fc56ddb99
Md. Shariful Islam <42964022+sisagor98@users.noreply.github.com>
2021-02-13 23:51:34 +0600

    collider add

---
+++ Assets/AreaCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCollider : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("Denger Area Inter");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("Denger Area Inter");
+        }
+    }
+}
